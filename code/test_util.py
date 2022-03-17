7cf606c511bc5d28308d00e84a2170d4ad18dfb7
ogison <59614846+ogison@users.noreply.github.com>
2022-03-17 13:33:05 +0900

    Update test_util.py

--- code/test_util.py
+++ code/test_util.py
@@ -1,20 +1,13 @@
-
 class Tester:
 
     def __init__(self, k=[5, 10, 20]):
-        self.k = k
+        self.k = [5]
         self.session_length = 19
         self.n_decimals = 4
         self.initialize()
 
     def initialize(self):
         self.i_count = [0]*19
-        # self.i_1_count = 0
-        # self.i_2_count = 0
-        # self.i_3_count = 0
-        # self.tar_1_count = 0
-        # self.tar_2_count = 0
-        # self.tar_3_count = 0
         self.recall = [[0]*len(self.k) for i in range(self.session_length)]
         self.mrr = [[0]*len(self.k) for i in range(self.session_length)]
 
@@ -40,33 +33,7 @@ class Tester:
 
             self.i_count[i] += 1
 
-    def evaluate_sequence_repeat(self, predicted_sequence, target_sequence, seq_len):
-        for i in range(seq_len):
-            target_item = target_sequence[i]
-            k_predictions = predicted_sequence[i]
-
-            if target_item == 1:
-                self.i_1_count +=1
-                if target_item == k_predictions:
-                    self.tar_1_count += 1
-            elif target_item == 2:
-                self.i_2_count +=1
-                if target_item == k_predictions:
-                    self.tar_2_count += 1
-            elif target_item == 3:
-                self.i_3_count +=1
-                if target_item == k_predictions:
-                    self.tar_3_count += 1
-
-            self.i_count[i] += 1
-
     def evaluate_batch(self, predictions, targets, sequence_lengths):
-        for batch_index in range(len(predictions)):
-            predicted_sequence = predictions[batch_index]
-            target_sequence = targets[batch_index]
-            self.evaluate_sequence(predicted_sequence, target_sequence, sequence_lengths[batch_index])
-
-    def evaluate_batch_repeat(self, predictions, targets, sequence_lengths):
         for batch_index in range(len(predictions)):
             predicted_sequence = predictions[batch_index]
             target_sequence = targets[batch_index]
@@ -74,12 +41,11 @@ class Tester:
 
     def format_score_string(self, score_type, score):
         tabs = '\t'
-        #if len(score_type) < 8:
-        #    tabs += '\t'
         return '\t'+score_type+tabs+score+'\n'
 
     def get_stats(self):
-        score_message = "Recall@5\tMRR@5\tRecall@10\tMRR@10\tRecall@20\tMRR@20\n"
+        # score_message = "Recall@5\tMRR@5\tRecall@10\tMRR@10\tRecall@20\tMRR@20\n"
+        score_message = "Recall@5\tMRR@5\n"
         current_recall = [0]*len(self.k)
         current_mrr = [0]*len(self.k)
         current_count = 0
@@ -103,13 +69,14 @@ class Tester:
                 mrr_k[j] = m
 
         recall5 = recall_k[0]
-        recall10 = recall_k[1]
-        recall20 = recall_k[2]
+        # recall10 = recall_k[1]
+        # recall20 = recall_k[2]
         mrr5 = mrr_k[0]
-        mrr10 = mrr_k[1]
-        mrr20 = mrr_k[2]
+        # mrr10 = mrr_k[1]
+        # mrr20 = mrr_k[2]
 
-        return score_message, recall5, recall10, recall20, mrr5, mrr10, mrr20
+        # return score_message, recall5, recall10, recall20, mrr5, mrr10, mrr20
+        return score_message, recall,mrr5
 
     def get_stats_and_reset(self):
         message = self.get_stats()
